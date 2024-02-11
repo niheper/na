61d68e8260cdae3e09b1cc91988fd2cd9b35e280
niheper <chivok2006@gmail.com>
2024-02-11 17:17:05 +0300

    Движение игрока

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour
     public float gravity = 9.8f;
     public float jumpForce;
 
-    private float _fallVelocity = 0;
+    public float speed;
 
+    private float _fallVelocity = 0;
+    
     private CharacterController _characterController;
+
+    private Vector3 _moveVector;
 
     void Start()
     {
@@ -18,13 +22,48 @@ public class PlayerController : MonoBehaviour
 
    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded) 
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
             _fallVelocity = -jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+       
     }
 
     void FixedUpdate()
     {
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity += gravity * Time.fixedDeltaTime;
+        }
+
         _fallVelocity += gravity * Time.fixedDeltaTime;
+
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
     }
 }
