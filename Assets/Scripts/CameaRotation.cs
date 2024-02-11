6b13b3c62e2f869de2e629816bc8a881abd78fce
niheper <chivok2006@gmail.com>
2024-02-11 15:10:07 +0300

    Движение по оси Y

--- Assets/Scripts/CameaRotation.cs
+++ Assets/Scripts/CameaRotation.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CameaRotation : MonoBehaviour
 {
     public float RotationSpeed;
+
+    public float minAngle;
+    public float maxAngle;
+
+    public Transform CameraAxisTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,15 @@ public class CameaRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+        var newAngleY = transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+
+        var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+            newAngleX -= 360;
+
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
     }
 }
