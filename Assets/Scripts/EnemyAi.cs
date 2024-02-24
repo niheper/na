58915eb7333ab036466c0ff73d32c79389fa65ba
niheper <chivok2006@gmail.com>
2024-02-24 15:30:34 +0300

    Enemy
    New Scene;
    Enemy;
    Path for enemy.

---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public Transform targetPoint;
+
+    private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.destination = targetPoint.position;
+
+    }
+}
